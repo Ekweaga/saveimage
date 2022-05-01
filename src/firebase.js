c2b1c70af73ca9ec481087c36fdebfafcbe58a06
Ekweaga <hikay133@gmail.com>
2022-05-01 01:14:39 +0100

    Authentication logic in place

--- src/firebase.js
+++ src/firebase.js
@@ -22,7 +22,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const projectfirestore = getFirestore(app)
-const projectstorage = getStorage(app)
+const projectstorage = getStorage(app);
+const auth = getAuth(app)
 
 
-export {projectstorage, projectfirestore}
+export {projectstorage, projectfirestore,auth}
--- src/signup.js
+++ src/signup.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
+import { auth } from './firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import './signup.css'
 
 function Signup() {
+  const [loading,setLoading] = useState(false)
+  const emailref = useRef();
+  const passwordref = useRef();
+ 
+  const  signup = async ()=>{
+    setLoading(true)
+    try{
+      await createUserWithEmailAndPassword(auth,emailref.current.value,passwordref.current.value);
+      setLoading(false)
+    }
+    catch(err){
+      console.log(err)
+    }
+ 
+  
+
+  }
+ 
   return (
     <div className='formtag'>
           <div style={{color:'white',background:'orangered',borderRadius:'50%',width:'100px',height:'100px', margin:'auto', marginTop:'100px',textAlign:'center',display:'flex',alignItems:'center',justifyContent:'center'}}> 
           <span style={{textAlign:'center',fontSize:'25px'}}>SOI</span> </div>
-        <form>
+        <form onSubmit={signup}>
             <div>
-                <input type="text" placeholder='Name'/>
+                <input type="email" placeholder='Email' ref={emailref}/>
             </div>
             <div>
-                <input type="email" placeholder='Email'/>
+                <input type="password" placeholder='Password' ref={passwordref}/>
             </div><br/>
             <div>
-              <button type="submit">SIGN UP</button>
+              <button type="submit" >{loading ? "CREATING...." :"SIGN UP"}</button>
 
             </div>
         </form>
